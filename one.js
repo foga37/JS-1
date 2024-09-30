72db1f4dee5570fdbe7f10ed65d77f4d0cd608b1
foga37 <foga3737@gmail.com>
2024-09-30 22:48:06 +0300

    first com

---
+++ one.js
@@ -0,0 +1,52 @@
+var a = 12345
+var b = 54321
+var c = 'qwert'
+var d = 'trewq'
+var e = 'asdfg'
+let f = 'wwwer'
+let g = 'glow'
+let h = 6789
+let j = 9876
+let k = 232310
+const l = 22
+const m = 'hi' 
+const bv = 'kol'
+const cc = 'non'
+const x = 54
+console.log(a, b, c, d, e)
+document.write ('<hr>')
+document.write(f)
+document.write ('<hr>')
+document.write(g)
+document.write ('<hr>')
+document.write(h)
+document.write ('<hr>')
+document.write(j)
+document.write ('<hr>')
+document.write(k)
+document.write ('<hr>')
+alert(l)
+confirm(m)
+prompt(bv)
+document.write(cc)
+document.write ('<hr>')
+document.write(x)
+document.write ('<hr>')
+let o = 'var - старий підхід оголошення змінної, було актуально до 2014 року'
+let p = 'let - змінну можно переписати'
+let po = 'const - змінну записали і не можемо переписати'
+let pol = 'змінні мають бути унікальні по ідентифікатору'
+let lop = 'декларація - це коли джава скрипту кажемо що ця змінна буде існувати'
+let koll = 'ініціалізація - це коли під цією змінною маємо щось конкретно записати'
+document.write(o)
+document.write ('<hr>')
+document.write(p)
+document.write ('<hr>')
+document.write(po)
+document.write ('<hr>')
+document.write(pol)
+document.write ('<hr>')
+document.write(lop)
+document.write ('<hr>')
+document.write(koll)
+document.write ('<hr>')
